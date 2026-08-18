e25d010e50ce70b3b9475b658a4100fb7685f49a
andre <dree1509luiz@gmail.com>
2026-08-18 11:38:45 -0300

    feat codificando os componentes e trabalhando com props e destruct

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import Aluno from "./components/Aluno"
+import Curso from "./components/Curso"
 import Dados from "./components/Dados"
+import Treinamento from "./components/Treinamento"
+
 const App = () => {
   return (
     //fragments
     <>
       <Aluno nome="Fiap" idade="30"/>
       <Dados usuario="teste" email="teste@email.com"/>
+      <Treinamento id="10" nome="andre" email="andre@email.com" telefone="1111111111"/>
+      <Curso id="11" nome="dre" descricao="lindo" local="sao paulo" data="hoje"/>
     </>
   )
 }
